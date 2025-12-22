191fe6c27ebb5733c0bd41b9c19db2e85a18a6ab
Suboohi-Khan <suboohiarif25@gmail.com>
2025-12-22 06:28:45 -0800

    New changes

--- src/index.js
+++ src/index.js
@@ -1,17 +1,92 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM, { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+let div = document.getElementById("root");
+
+let Heading = () => {
+  return(
+    <h1>Heading Component</h1>
+  )
+}
+
+let Paragraph = () => {
+  return(
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Perferendis repudiandae sed voluptates accusantium incidunt vitae inventore ad in nulla commodi?</p>
+  )
+}
+
+let List = () => {
+  return(
+      <ul>
+                <li>Book</li>
+                <li>Pen</li>
+                <li>Laptop</li>
+                <li>HeadPhones</li>
+                <li>Plant</li>
+                <li>Coffee</li>
+                <li>Coffee Mug</li>
+            </ul>
+  )
+}
+
+let Table = () => {
+  return(
+     <table border="1" cellpadding="10" cellspacing="0">
+  <thead>
+    <tr>
+      <th>Item No</th>
+      <th>Item Name</th>
+      <th>Quantity</th>
+      <th>Price ($)</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>1</td>
+      <td>Book</td>
+      <td>5</td>
+      <td>50</td>
+    </tr>
+    <tr>
+      <td>2</td>
+      <td>Pen</td>
+      <td>10</td>
+      <td>15</td>
+    </tr>
+    <tr>
+      <td>3</td>
+      <td>Backpack</td>
+      <td>2</td>
+      <td>80</td>
+    </tr>
+    <tr>
+      <td>4</td>
+      <td>Laptop</td>
+      <td>1</td>
+      <td>800</td>
+    </tr>
+    <tr>
+      <td>5</td>
+      <td>Headphones</td>
+      <td>3</td>
+      <td>120</td>
+    </tr>
+  </tbody>
+</table>
+  )
+}
+
+createRoot(div).render(
+<div>
+ <Heading/>
+ <Paragraph/>
+ <List/>
+ <Table/>
+
+</div>
+
+)
